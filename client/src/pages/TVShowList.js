1ce071fb104b3b27f426a78bf8664aa50c3ba5cc
Brandon Tyson <betyson@student.fullsail.edu>
2025-05-20 23:57:45 -0400

    DONE

--- client/src/pages/TVShowList.js
+++ client/src/pages/TVShowList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../styles/style.css';
+//import { config } from 'dotenv';
+//config();
 
 const API_URL = process.env.REACT_APP_API_URL;
 
