57732a68e488f19f0373f4ef78006bea0013184d
Brandon Tyson <betyson@student.fullsail.edu>
2025-05-20 23:26:51 -0400

    API updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
+
 const connectDB = async () => {
+    const URI = process.env.MONGODB_URI;
+    console.log(URI + " URI");
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tvshows');
+         const conn = await mongoose.connect(URI || 'mongodb://localhost:27017/tvshows');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
